77b762461b101a113c36a4f502de2207d655dd00
Olwethu Mukona <siyaolwethu433@gmail.com>
2026-04-09 13:55:44 +0200

    fix: syntax error
    ProjectChatApp.java
     - Incorrect regular expressions for password validation
     - Relational operator error in password conditions
     - Cellphone format validation

--- ProjectChatApp/src/projectchatapp/ProjectChatApp.java
+++ ProjectChatApp/src/projectchatapp/ProjectChatApp.java
@@ -19,17 +19,17 @@ public class ProjectChatApp {
             System.out.println("Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.");
         }
         // At least 8 characters, capital letter, number & special character
-        boolean hasCapital = password.matches("[A-Z]");
-        boolean hasNumber = password.matches("\\d.");
-        boolean hasSpecial = password.matches("[^a-zA-Z0-9]");
-        if (password.length() => 8 && hasCapital == true && hasNumber == true && hasSpecial == true) {
+        boolean hasCapital = password.matches(".*[A-Z].*");
+        boolean hasNumber = password.matches(".*\\d.*");
+        boolean hasSpecial = password.matches(".*[^a-zA-Z0-9].*");
+        if (password.length() >= 8 && hasCapital == true && hasNumber == true && hasSpecial == true) {
             System.out.println("Password successfully captured.");
         } else {
             System.out.println("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number and a special character.");
         }
         // Number contains international country code followed by number
-        cellphoneFormat = cellphone.matches("^(\\\\+\\\\d{1,3}( )?)?((\\\\(\\\\d{1,3}\\\\))|\\\\d{1,3})[- .]?\\\\d{3,4}[- .]?\\\\d{4}$"); // International format
-        if (cellphoneFormat == cellphone) {
+        boolean cellphoneFormat = cellphone.matches("^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$"); // International format
+        if (cellphoneFormat == true) {
             System.out.println("Cell phone number successfully added.");
         } else {
             System.out.println("Cell phone number incorrectly formatted or does not contain international code.");
